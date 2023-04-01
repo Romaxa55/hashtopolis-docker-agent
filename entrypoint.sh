e03c05847edee03d55097ffdeb707ad9dad5de37
Roman SHamagin <roman.shamagin@7vpn.com>
2023-04-02 00:40:03 +0300

    init

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# скачиваем файл hashtopolis.zip по ссылке и сохраняем его в /app
+curl -L -o /app/hashtopolis.zip "$AGENT_URL"
+
+# переходим в /app и распаковываем hashtopolis.zip
+cd /app
+unzip hashtopolis.zip
+
+VOUCHER=$(head /dev/urandom | tr -dc A-Za-z | head -c $(($(od -An -N2 -i /dev/random) % 7 + 6)))
+echo $VOUCHER
+
+# запускаем Hashtopolis агента
+python3 hashtopolis.zip
