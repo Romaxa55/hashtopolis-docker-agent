fe7ee0b5351991f6084d352df8021338cb87a4e4
Roman SHamagin <roman.shamagin@7vpn.com>
2023-04-02 02:22:51 +0300

    add README.md and LICENSE.md

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ curl -L -o /app/hashtopolis.zip "$AGENT_URL"
 # переходим в /app и распаковываем hashtopolis.zip
 cd /app
 
-bash
 VOUCHER=$(head /dev/urandom | tr -dc A-Za-z | head -c $(($(od -An -N2 -i /dev/random) % 7 + 6)))
 
 
