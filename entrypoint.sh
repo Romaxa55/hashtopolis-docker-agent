8d7f2fece0356f910be70cfa1aac76fe0f9b3779
Roman SHamagin <roman.shamagin@7vpn.com>
2023-04-02 01:03:03 +0300

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,9 @@ curl -L -o /app/hashtopolis.zip "$AGENT_URL"
 cd /app
 unzip hashtopolis.zip
 
-pip3 install setuptools requests psutil
+pip3 install setuptools
+pip3 install requests
+pip3 install psutil
 
 
 #VOUCHER=$(head /dev/urandom | tr -dc A-Za-z | head -c $(($(od -An -N2 -i /dev/random) % 7 + 6)))
